8b2fafca47c039ca2ba804f6121a260bbe8b55da
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2026-03-09 18:01:56 +0900

    add:誕生日登録画面を作成

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,19 @@
 class CatsController < ApplicationController
+  def index
+  @cats = cats.all
+  end
+
+  def new
+    @cats = Cat.new
+  end
+
+  def create
+    @cats = cats.new(cats_params)
+    @cats.save
+  end
+
+  private
+def cats_params
+  params.require(:cat).permit(:name, :birthday)
+end
 end
