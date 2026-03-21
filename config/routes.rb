fca55b231e797aceb67b107bede4099c06e7f7c8
yashimamisaki <mokomoko0410gin@yahoo.co.jp>
2026-03-22 00:21:52 +0900

    fix:userモデル読み込み→device読み込み

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  #devise_for :users
   root "static_pages#top"
   resources :cats
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
